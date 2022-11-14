9528659e482b35eaa7b547cb19493e76b1e14f88
lethanhthaidn <89528318+lethanhthaidn@users.noreply.github.com>
2022-11-14 17:58:11 +0700

    thailt

---
+++ A12_Project_BE/A12_Project_BE/src/main/java/codegym/a12_project_be/A12ProjectBeApplication.java
@@ -0,0 +1,13 @@
+package codegym.a12_project_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A12ProjectBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A12ProjectBeApplication.class, args);
+    }
+
+}
---
+++ A12_Project_BE/A12_Project_BE/src/test/java/codegym/a12_project_be/A12ProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package codegym.a12_project_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A12ProjectBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
